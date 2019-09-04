a7f9e8c03f99206bbd14b11731b809d1311828df
christianbrandenburg <pinconspiracy@gmail.com>
2019-09-04 08:39:55 +0000

    done opg

---
+++ firewall.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+az extension add -n azure-firewall
+
+az group create --name Test-FW-RG --location northeurope
+
+az network vnet create \
+  --name Test-FW-VN \
+  --resource-group Test-FW-RG \
+  --location northeurope \
+  --address-prefix 10.0.0.0/16 \
+  --subnet-name AzureFirewallSubnet \
+  --subnet-prefix 10.0.1.0/26
+az network vnet subnet create \
+  --name Workload-SN \
+  --resource-group Test-FW-RG \
+  --vnet-name Test-FW-VN   \
+  --address-prefix 10.0.2.0/24
+az network vnet subnet create \
+  --name Jump-SN \
+  --resource-group Test-FW-RG \
+  --vnet-name Test-FW-VN   \
+  --address-prefix 10.0.3.0/24
+
+az vm create \
+    --resource-group Test-FW-RG \
+    --name Srv-Jump \
+    --location northeurope \
+    --image win2016datacenter \
+    --vnet-name Test-FW-VN \
+    --subnet Jump-SN \
+    --admin-username azureadmin
+az vm open-port --port 3389 --resource-group Test-FW-RG --name Srv-Jump
+
+az network nic create \
+    -g Test-FW-RG \
+    -n Srv-Work-NIC \
+   --vnet-name Test-FW-VN \
+   --subnet Workload-SN \
+   --public-ip-address "" \
+   --dns-servers 209.244.0.3 209.244.0.4
+
+az vm create \
+    --resource-group Test-FW-RG \
+    --name Srv-Work \
+    --location northeurope \
+    --image win2016datacenter \
+    --nics Srv-Work-NIC \
+    --admin-username azureadmin
+
+az network firewall create \
+    --name Test-FW01 \
+    --resource-group Test-FW-RG \
+    --location northeurope
+az network public-ip create \
+    --name fw-pip \
+    --resource-group Test-FW-RG \
+    --location northeurope \
+    --allocation-method static \
+    --sku standard
+az network firewall ip-config create \
+    --firewall-name Test-FW01 \
+    --name FW-config \
+    --public-ip-address fw-pip \
+    --resource-group Test-FW-RG \
+    --vnet-name Test-FW-VN
+az network firewall update \
+    --name Test-FW01 \
+    --resource-group Test-FW-RG 
+az network public-ip show \
+    --name fw-pip \
+    --resource-group Test-FW-RG
+fwprivaddr="$(az network firewall ip-config list -g Test-FW-RG -f Test-FW01 --query "[?name=='FW-config'].privateIpAddress" --output tsv)"
+
+az network route-table create \
+    --name Firewall-rt-table \
+    --resource-group Test-FW-RG \
+    --location northeurope \
+    --disable-bgp-route-propagation true
+
+az network route-table route create \
+  --resource-group Test-FW-RG \
+  --name DG-Route \
+  --route-table-name Firewall-rt-table \
+  --address-prefix 0.0.0.0/0 \
+  --next-hop-type VirtualAppliance \
+  --next-hop-ip-address $fwprivaddr
+
+az network vnet subnet update \
+    -n Workload-SN \
+    -g Test-FW-RG \
+    --vnet-name Test-FW-VN \
+    --address-prefixes 10.0.2.0/24 \
+    --route-table Firewall-rt-table
+
+az network firewall application-rule create \
+   --collection-name App-Coll01 \
+   --firewall-name Test-FW01 \
+   --name Allow-Google \
+   --protocols Http=80 Https=443 \
+   --resource-group Test-FW-RG \
+   --target-fqdns www.google.com \
+   --source-addresses 10.0.2.0/24 \
+   --priority 200 \
+   --action Allow
+
+az network firewall network-rule create \
+   --collection-name Net-Coll01 \
+   --destination-addresses 209.244.0.3 209.244.0.4 \
+   --destination-ports 53 \
+   --firewall-name Test-FW01 \
+   --name Allow-DNS \
+   --protocols UDP \
+   --resource-group Test-FW-RG \
+   --priority 200 \
+   --source-addresses 10.0.2.0/24 \
+   --action Allow
+
+az vm list-ip-addresses \
+    -g Test-FW-RG \
+    -n Srv-Work
